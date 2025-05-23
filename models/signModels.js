2a5101756e3b62c318e830a6d4d13c8c7d4de2d2
bivekk51 <nepaldynasty08@gmail.com>
2025-05-23 12:28:02 +0545

    more features apparently

--- models/signModels.js
+++ models/signModels.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const signSchema = new mongoose.Schema({
   label: { type: String, required: true },
-  language: { type: String, required: true }, // "asl" or "nsl"
+  language: { type: String, required: true,default:'asl' }, // "asl" or "nsl"
   image: { type: String, required: true },    // base64 string of the image
+  type:{type:String,default: 'letter'}
 }, { timestamps: true });
 
 const Sign = mongoose.model("Sign", signSchema);
